112184ebe5bc323a047ddf212a2802a861cdef04
LEx Klimin (lex-nel) <lex.klimin@gmail.com>
2022-09-29 11:37:18 +0700

    feat: project changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
-import { CommentController } from './controllers/comment.controller';
-import { PostController } from './controllers/post.controller';
-import { UserController } from './controllers/user.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { PrismaService } from './services/prisma.service';
+import { UserModule } from './modules/user/user.module';
+import { PostModule } from './modules/post/post.module';
+import { CommentModule } from './modules/comment/comment.module';
+import { TagModule } from './modules/tag/tag.module';
 
 @Module({
-  imports: [],
-  controllers: [UserController, PostController, CommentController],
+  imports: [
+    UserModule,
+    PostModule,
+    CommentModule,
+    TagModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+  ],
+  controllers: [],
   providers: [PrismaService],
 })
 export class AppModule {}
